f6f82e7c087a3d6245c80ef007ec5e1b6951a486
Alejandro Sánchez <alejo-lopera@hotmail.com>
2025-01-21 23:56:04 -0500

    lambda con api gateway

--- lambda-cdk/lib/lambda-cdk-stack.ts
+++ lambda-cdk/lib/lambda-cdk-stack.ts
@@ -1,16 +1,23 @@
-import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class LambdaCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class LambdaCdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+    
+    const hello = new Function(this, 'HelloHandler', {
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset('lambda'),
+      handler: 'hello.handler'
+    });
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'LambdaCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    new LambdaRestApi(this, 'ApiGateway', {
+      handler: hello
+    });
+    
   }
 }
