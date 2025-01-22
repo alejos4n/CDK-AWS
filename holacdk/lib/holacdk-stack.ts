db941fcefcd197ef4ebb4eb4a9a1cbf5b69d0d83
Alejandro Sánchez <alejo-lopera@hotmail.com>
2025-01-21 22:50:46 -0500

    desplegando sqs

--- holacdk/lib/holacdk-stack.ts
+++ holacdk/lib/holacdk-stack.ts
@@ -1,16 +1,35 @@
-import * as cdk from 'aws-cdk-lib';
+import { aws_s3, aws_s3_notifications, Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { Bucket, BucketProps } from 'aws-cdk-lib/aws-s3';
+import { Queue, QueueProps } from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class HolacdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class HolacdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Definición de propiedades para la cola (SQS)
+    const queueProps: QueueProps = {
+      visibilityTimeout: Duration.seconds(300), // Timeout de visibilidad de 5 minutos
+    };
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'HolacdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Creación de la cola
+    const queue = new Queue(this, 'HelloCdkQueue', queueProps);
+
+    // Definición de propiedades para el bucket (S3)
+    const bucketProps: BucketProps = {
+      versioned: true, // Habilitar versionado
+      removalPolicy: RemovalPolicy.DESTROY, // Eliminar el bucket al destruir la pila
+      autoDeleteObjects: true, // Borrar los objetos automáticamente al destruir el bucket
+    };
+
+    // Creación del bucket
+    const newBucket = new Bucket(this, 'MyFirstBucket', bucketProps);
+
+    // Ejemplo de posible integración entre S3 y SQS (opcional)
+    // Puedes añadir una notificación para enviar mensajes a SQS cuando ocurran eventos en el bucket
+    newBucket.addEventNotification(
+      aws_s3.EventType.OBJECT_CREATED,
+      new aws_s3_notifications.SqsDestination(queue)
+    );
   }
 }
